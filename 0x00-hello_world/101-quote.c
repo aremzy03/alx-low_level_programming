7cd19c4be71b540649743529b6b35a1d2be8e6b4
aremzy03 <aremzy2018@gmail.com>
2023-03-16 18:31:51 +0000

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korper, 2015-10-19");
+	perror("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 	return (1);
 }
