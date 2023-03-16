849692fd0f7270955bc1b7b54ba13a63c07a4164
aremzy03 <aremzy2018@gmail.com>
2023-03-16 18:21:53 +0000

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <errno.h>
+/**
+ *main - print a message in standard error
+ *
+ *Return: 1
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - DoraKerper, 2015-10-19");
+	return (1);
+}
