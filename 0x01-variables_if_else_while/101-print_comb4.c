45b64ba133d985c78dd0c6ac0b3e0b83293502a0
aremzy03 <aremzy2018@gmail.com>
2023-03-17 23:48:20 +0000

    101 print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+/**
+ *main - main function
+ *
+ *Return: 0
+ */
 int main(void)
 {
 	int a, b, c;
+
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10 ; b++)
+		for (b = 1; b < 10 ; b++)
 		{
-			for (c = 0; c < 10; c++)
+			for (c = 2; c < 10; c++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(c + '0');
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
