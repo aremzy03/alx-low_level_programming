46b9692ef8e96831f29633da7dee518d2737370c
aremzy03 <aremzy2018@gmail.com>
2023-03-17 21:52:38 +0000

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - prints combinations of one numbers
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int comb;
+
+	comb = 0;
+	while (comb < 10)
+	{
+		putchar(comb + '0');
+		putchar(',');
+		putchar(' ');
+		comb++;
+	}
+	return (0);
+}
