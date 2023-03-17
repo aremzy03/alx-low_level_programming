13e762690c3a39d57030a948b4f96133d7201bb0
aremzy03 <aremzy2018@gmail.com>
2023-03-17 16:45:42 +0000

    printed alphabets in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - prints a -z in both upper and lower case
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	lower = 'a';
+	upper = 'A';
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+	return (0);
+}
