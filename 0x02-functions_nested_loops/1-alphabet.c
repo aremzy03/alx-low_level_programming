398c582296184f3dac6e415e08dcfe91ce3a92b4
aremzy03 <aremzy2018@gmail.com>
2023-03-21 15:34:06 +0000

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/**
+ *print_alphabet - display all the alphabets
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
