8e815d1b8bbb0976c6f8b9b371880fbd7dd11151
aremzy03 <aremzy2018@gmail.com>
2023-03-21 18:48:03 +0000

    prints absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_abs - prints the absolute number of an integer
+ *@num: is the arguement for the function
+ *
+ *Return: 0
+ */
+int _abs(int num)
+{
+	int absolute;
+
+	absolute = abs(num);
+	return (absolute);
+}
