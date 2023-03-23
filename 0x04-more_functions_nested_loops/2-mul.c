3d2d4795c6cbb517a66a368746a0c298032e881e
aremzy03 <aremzy2018@gmail.com>
2023-03-23 12:52:16 +0000

    multiplication funtion

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - returns the product of a and b
+ *@a: the first arguement
+ *@b: the second arguement
+ *
+ *Return: the product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
