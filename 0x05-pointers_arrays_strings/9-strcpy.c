c2ced89981d0b3bec56bbcdadff169b8edf997dd
aremzy03 <aremzy2018@gmail.com>
2023-03-27 22:44:47 +0000

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcpy - copies string to dest
+ *@dest: the location of the copies string
+ *@src: the string to be copied
+ *
+ *Return: the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
