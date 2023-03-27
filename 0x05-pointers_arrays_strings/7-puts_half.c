4a488a380bca5b548d652fac5c5ee1393aab6407
aremzy03 <aremzy2018@gmail.com>
2023-03-27 21:19:43 +0000

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+ *puts_half - puts half of a string array
+ *@str: the string to be printed
+ *
+ *Return: nil
+ */
+void puts_half(char *str)
+{
+	unsigned long int i;
+	unsigned long int len, half_len, half_lenodd;
+
+	len = strlen(str);
+	half_len = len / 2;
+	half_lenodd = (len - 1) / 2;
+	if (strlen(str) % 2 != 0)
+	{
+		for (i = half_lenodd; i <= len - 1; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	else
+	{
+		for (i = half_len; i <= len - 1; i++)
+		{
+			printf("%c", str[i]);
+		}
+	}
+	putchar('\n');
+}
