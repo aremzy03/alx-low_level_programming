e2c02d1260fcdb6447cf699b32536839edba2fcf
aremzy03 <aremzy2018@gmail.com>
2023-03-27 15:49:38 +0000

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - returns the length of the string s
+ *@s: the arguement for the string
+ *
+ *Return: the length of string s
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
