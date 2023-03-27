3c24a70a164017cfb845b3504165b995ce30fde9
aremzy03 <aremzy2018@gmail.com>
2023-03-27 14:49:12 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - swaps the value of a and b
+ *@a: the first arguement
+ *@b: the second arguement
+ *
+ *Return: none
+ */
+void swap_int(int *a, int *b)
+{
+
+	*a = 42;
+	*b = 92;
+}
