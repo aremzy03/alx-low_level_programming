7b21e57cb4c1d55565d01b1b53375be261cd15f1
aremzy03 <aremzy2018@gmail.com>
2023-03-30 13:24:37 +0000

    strcompare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - compares the two strings
+ *@s1: the first string to be compared
+ *@s2: the second string to be compared
+ *
+ *Return: returns the comparative of the two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
