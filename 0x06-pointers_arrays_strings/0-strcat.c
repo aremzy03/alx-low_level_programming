d28a6f0bb201e2cee8ae4c5be3914dbc0e743673
aremzy03 <aremzy2018@gmail.com>
2023-03-30 12:05:52 +0000

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcat - returns the a concatinated string
+ *@dest: the first string to be concatinated
+ *@src: the second string to be concatinated
+ *
+ *Return: the concatinated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
