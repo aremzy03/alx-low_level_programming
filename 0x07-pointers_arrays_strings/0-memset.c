4dd8de9a885fe603619a6403e4b520770a8dc163
aremzy03 <aremzy2018@gmail.com>
2023-04-03 09:41:08 +0000

    made memset betty compliant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 #include <string.h>
+/**
+ *_memset - returns memset of an array
+ *@s: this where the array is pointed to
+ *@b: the constant byte
+ *@n: the first number of bytes of memory area
+ *
+ *Return: memset
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	return (memset(s, b, n));
