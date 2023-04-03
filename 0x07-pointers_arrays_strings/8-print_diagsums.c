d7303ed19ac4a3879f408a967d72a60ada6d1cac
aremzy03 <aremzy2018@gmail.com>
2023-04-03 12:58:15 +0000

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,13 +10,14 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int dia1, dia2;
+	int dia1, dia2, sizesq;
 
-	for (i = 0; i <= size * size; i = i + size + 1)
+	sizesq = size * size;
+	for (i = 0; i <= sizesq; i = i + size + 1)
 	{
 		dia1 += a[i];
 	}
-	for (j = size - 1; j <= size * size - size; j = j + size - 1)
+	for (j = size - 1; j <= sizesq - size; j = j + size - 1)
 	{
 		dia2 += a[j];
 	}
