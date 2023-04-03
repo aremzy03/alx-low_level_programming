5a868b4c58f0795aa34e48af8e722e571412d79c
aremzy03 <aremzy2018@gmail.com>
2023-04-03 12:47:00 +0000

    diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums - prints the sum of the diagonal of a square
+ *@a: the array to be printed
+ *@size: the size of the array
+ *
+ *Return: none
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int dia1, dia2;
+
+	for (i = 0; i <= size * size; i = i + size + 1)
+	{
+		dia1 += a[i];
+	}
+	for (j = size - 1; j <= size * size - size; j = j + size - 1)
+	{
+		dia2 += a[j];
+	}
+	printf ("%d, %d\n", dia1, dia2);
+}		
