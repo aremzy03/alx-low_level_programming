397af8dfbd7d60f437eddbf1f26df3a55829790c
aremzy03 <aremzy2018@gmail.com>
2023-04-05 14:11:12 +0000

    fixed betty error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ *_puts_recursion - prints the character s using a recursive function
+ *@s: the string to be printed
+ *
+ *Return: no return value
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
