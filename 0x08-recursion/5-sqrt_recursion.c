85d66903394de89081111cfc8946e6833602830a
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-06 01:21:47 +0000

    square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,29 @@
 #include "main.h"
+/**
+ *sqroot - finds the square root of n
+ *@n: the arguement
+ *@i: the counter
+ *
+ *Return: the square root
+ */
+int sqroot(int n, int i)
+{
+	int squroot;
+
+	squroot = i * i;
+	if (n < squroot)
+		return (-1);
+	if (n == squroot)
+		return (i);
+	return (sqroot(n, i + 1));
+}
+/**
+ *_sqrt_recursion - prints the square root
+ *@n: the arguement
+ *
+ *Return: the square root
+ */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
-	{
-		return (1);
-	}
-	
+	return (sqroot(n, 1));
+}
