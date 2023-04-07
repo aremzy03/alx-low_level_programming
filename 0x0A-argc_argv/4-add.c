662f814dba12ab52fbd775e2dda1d42ec6e44660
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-07 16:36:46 +0000

    addition of arguements

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints the sum of the arguements
+ *@argc: the number of arguements
+ *@argv: the arguement list
+ *
+ *Return: zero(0) if success and one(1) if failed.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
