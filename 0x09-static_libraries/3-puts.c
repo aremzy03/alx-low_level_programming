a7e2a5fa2712fe1850be26f462236f702bc852c4
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-09 11:54:07 +0000

    created static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *_puts - prints out a string
- *@str: the string to be outputted
+ *@s: the string to be outputted
  *
  *Return: nil
  */
-void _puts(char *str)
-	#include<stdio.h>
-/**
- *main - Entry point
- *
- *Return: Always 0 (Success)
- */
-	int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts(s);
 }
