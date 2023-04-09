2510b16e36d635e671087c6c27c046917864c40b
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-09 11:41:02 +0000

    fixed betty error

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,16 @@
 #include "main.h"
+/**
+ *_puts - prints out a string
+ *@str: the string to be outputted
+ *
+ *Return: nil
+ */
 void _puts(char *str)
 	#include<stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 	int main(void)
 {
