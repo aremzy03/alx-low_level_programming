e37200ddf0b3854eef6604d5b3fda8c19639b18b
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-11 14:42:40 +0000

    added a readme file and first task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *create_array - returns an array of a size
+ *@size: the size of the array
+ *@c: the character of the array;
+ *
+ *Return: the pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr_ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	arr_ptr = malloc(size);
+	for (i = 0; i < size; i++)
+	{
+		arr_ptr[i] = c;
+	}
+	return (arr_ptr);
+}
