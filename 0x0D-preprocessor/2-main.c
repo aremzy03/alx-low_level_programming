f36f46536e3868f929f7d44fa9b20ca1102974ed
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-17 22:02:28 +0000

    print source file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,4 @@
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include <stdio.h>
 /**
  *main - prints the file name of the program
  *
@@ -17,13 +6,6 @@ int _putchar(char c)
  */
 int main(void)
 {
-	const char *file  = __FILE__;
-
-	while (*file)
-	{
-		_putchar(*file);
-		file++;
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
