98d39d578dec83ed058023e3167de33a75474529
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-20 06:29:46 +0000

    fixed betty error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,10 @@
 #include "function_pointers.h"
+/**
+ *array_iterator - executes the function for each array
+ *@array: the array containing the parameters of the functions
+ *@size: the size of the array
+ *@action: the actions taken in the each element of array
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
