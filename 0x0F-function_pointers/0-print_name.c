385929981ff46adb32eabe2cc300211bd1120e49
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-20 06:02:09 +0000

    added readme and first project task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints name in function
+ *@name: the name to be printed
+ *@f: the function to return name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
