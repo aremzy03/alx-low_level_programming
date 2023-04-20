3e12ea03033714bed80a138de31a16f71b3556f7
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-04-20 06:32:03 +0000

    array iteration

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	for (i = 0; i < size; i++)
+	if (array)
 	{
-		if (array[i] && action)
-			action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] && action)
+				action(array[i]);
+		}
 	}
 }
