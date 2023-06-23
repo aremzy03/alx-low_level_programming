b5c9ecb46537a859037efd87eb760d163ecb6417
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-06-23 23:31:07 +0000

    another commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,4 +3,16 @@
 #include <stdarg.h>
 void print_all(const char * const format, ...)
 {
-	
+	int i;
+	va_list format;
+	for (i = 0, format[i] != '\0'; i++)
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c",va_arg(format, char));
+		case 'i':
+			printf(va_arg(format, int));
+			case ''
+		}
+	}
