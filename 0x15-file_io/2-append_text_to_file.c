33903d9b9773df70df48b0ec31451e2855850f23
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-09 09:06:12 +0000

    fixed betty error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+/**
+ *append_text_to_file - appends a text to the end of a file
+ *@filename: the name of the file to be appended
+ *@text_content: the content to be appended
+ *
+ *Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
