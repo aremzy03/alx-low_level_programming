3141ada2a4628c6f3a33d92db6070f51768ff672
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-09 01:39:22 +0000

    check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 
 	if (filename == NULL)
-		return (-1);
+		return (STDERR_FILENO);
 	if (text_content == NULL)
 		text_content = "";
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
