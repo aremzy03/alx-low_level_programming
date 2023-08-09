81364edbfa237a9267d856eba13e9d07044ca999
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-09 01:36:09 +0000

    made some changes to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  *create_file - creates a file and write to the file
  *@filename: the name of the file to be created
@@ -11,14 +11,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fd;
+	int fd;
 
 	if (filename == NULL)
-		return (STDERR_FILENO);
+		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = fopen(filename, "w+");
-	fprintf(fd, "%s", text_content);
-	fclose(fd);
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	write(fd, text_content, strlen(text_content));
+	close(fd);
 	return (1);
 }
