93e010a99aecb374d25bd01f248ce8562de0e0cd
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-11 21:26:11 +0000

    and another bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w;
 
 	if (filename == NULL)
-		return (STDERR_FILENO);
+		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 	fd = open(filename, O_RDWR | O_CREAT, 0600);
-	write(fd, text_content, strlen(text_content));
+	if (fd == -1)
+		return (-1);
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
