d2a3242ada7d29705b18e12ebb75bcbbf1e62e5d
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-11 18:16:16 +0000

    fixed betty error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,12 +14,12 @@ char *create_buffer(char *file)
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-	if(buffer == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
-	return(buffer);
+	return (buffer);
 }
 /**
  *close_file - closes the files
@@ -44,7 +44,6 @@ void close_file(int fd, int fd2)
 		exit(100);
 	}
 }
-	
 /**
  *main - reads the content of a file and writes it to another file
  *@ac: the number of files to work with
@@ -66,16 +65,15 @@ int main(int ac, char **av)
 	src = open(av[1], O_RDONLY);
 	des = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	r = read(src, Buff, 1024);
-	do
-	{
-		if(src == -1 || r == -1)
+	do {
+		if (src == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			free(Buff);
 			exit(98);
 		}
 		w = write(des, Buff, r);
-		if (des == -1 ||w == -1)
+		if (des == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			free(Buff);
@@ -83,8 +81,7 @@ int main(int ac, char **av)
 		}
 		r = read(src, Buff, 1024);
 		des = open(av[2], O_WRONLY | O_APPEND);
-	}
-	while(r > 0);
+	} while (r > 0);
 	free(Buff);
 	close_file(src, des);
 	return (0);
