225a185d253a9020b505854fda2a818afd15f447
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-11 18:11:34 +0000

    fixed more bugs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,48 @@
 #include "main.h"
 #include <string.h>
 /**
+ *create_buffer - creates a buffer and allocates memory of 1024bytes
+ *@file: file of buffer to be allocated
+ *
+ *Return: the buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if(buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return(buffer);
+}
+/**
+ *close_file - closes the files
+ *@fd: the first file
+ *@fd2: the second file
+ *
+ */
+void close_file(int fd, int fd2)
+{
+	int cls, cls2;
+
+	cls = close(fd);
+	if (cls == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	cls2 = close(fd2);
+	if (cls2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+	
+/**
  *main - reads the content of a file and writes it to another file
  *@ac: the number of files to work with
  *@av: the src file and the des file
@@ -12,42 +54,38 @@
  */
 int main(int ac, char **av)
 {
-	int src, des, clse;
-	char Buff[1024];
+	int src, des, w, r;
+	char *Buff;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	Buff = create_buffer(av[2]);
 	src = open(av[1], O_RDONLY);
-	if (src == -1)
+	des = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	r = read(src, Buff, 1024);
+	do
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		if(src == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",av[1]);
+			free(Buff);
+			exit(98);
+		}
+		w = write(des, Buff, r);
+		if (des == -1 ||w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(Buff);
+			exit(99);
+		}
+		r = read(src, Buff, 1024);
+		des = open(av[2], O_WRONLY | O_APPEND);
 	}
-	des = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (des == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	while (read(src, Buff, strlen(Buff)) > 0)
-	{
-		write(des, Buff, strlen(Buff));
-	}
-	write(des, "\n", 1);
-	clse = close(src);
-	if (clse == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", src);
-		exit(100);
-	}
-	clse = close(des);
-	if (clse == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", des);
-		exit(100);
-	}
+	while(r > 0);
+	free(Buff);
+	close_file(src, des);
 	return (0);
 }
