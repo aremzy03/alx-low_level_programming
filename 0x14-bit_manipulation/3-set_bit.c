a63300c9f91682d65095f14f6a9e6323bf1f8efc
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-08-21 09:30:15 +0000

    fixed another betty error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,15 @@
 #include "main.h"
+/**
+ *set_bit - set value of bit to 1 at given index
+ *@n: the number to change
+ *@index: the index of the value
+ *
+ *Return: value that was set
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 	*n = ((1UL <<  index) | *n);
-	return(1);
+	return (1);
 }
