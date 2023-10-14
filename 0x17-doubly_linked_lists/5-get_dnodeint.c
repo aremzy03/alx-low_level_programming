d5a2ca5d0b9c03c9424bd62b893150e07ecac022
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-14 23:12:38 +0100

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,22 +1,5 @@
 #include "lists.h"
 /**
- * dlistint_len - returns the length of a linked list
- * @h: the linked list to to check
- *
- * Return: the length of the linked list
-*/
-size_t dlistint_len(const dlistint_t *h)
-{
-	int count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-/**
  * get_dnodeint_at_index - gets the nodes of linked list at an index
  * @head: the pointer to the linked list
  * @index: the index at of the linked list to be fetch
