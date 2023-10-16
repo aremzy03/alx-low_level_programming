bf763c18ba1827937eb218c7ee1fbd86f35c7cc8
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-16 12:06:03 +0100

    100-tests.py

---
+++ 0x18-dynamic_libraries/py.c
@@ -0,0 +1,56 @@
+#include "py.h"
+/**
+ * add - adds a and b
+ * @a: the first value
+ * @b: the second value
+ *
+ * Return: sum
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - adds a and b
+ * @a: the first value
+ * @b: the second value
+ *
+ * Return: difference
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - adds a and b
+ * @a: the first value
+ * @b: the second value
+ *
+ * Return: multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - adds a and b
+ * @a: the first value
+ * @b: the second value
+ *
+ * Return: division
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - adds a and b
+ * @a: the first value
+ * @b: the second value
+ *
+ * Return: modulus
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
